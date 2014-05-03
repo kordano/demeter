09dd0d007c071ee435e53a2c391b3743570abb0b
kordano <konrad.kuehne@rocketmail.com>
2014-05-03 13:09:00 +0200

    revamp handler for sided timeline and detail view

--- src/demeter/server/CoreHandler.java
+++ src/demeter/server/CoreHandler.java
@@ -7,32 +7,30 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 public class CoreHandler extends AbstractHandler{
-	final String _greeting;
-	final String _body;
+	private static String _body;
+	private static Compositer compositer = null;
 	 
 	public CoreHandler() {
-		_greeting = "Hello World";
 		_body = null;
 	}
 	 
-	public CoreHandler(String greeting) {
-		_greeting = greeting;
-		_body = null;
-	}
 	 
-	public CoreHandler(String greeting, String body) {
-		_greeting = greeting;
-		_body = body;
+	public CoreHandler(String username) {
+		compositer = new Compositer(username);
+		_body = null;
 	}
 	 
 	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		response.setContentType("text/html;charset=utf-8");
 		response.setStatus(HttpServletResponse.SC_OK);
 		baseRequest.setHandled(true);
+		if(request.getParameter("tweetID") != null) {
+			_body = compositer.buildPage(request.getParameter("tweetID"));
+		} else {
+			_body = compositer.buildPage();
+		}
 		if (_body != null) response.getWriter().println(_body);
 	}
 }
